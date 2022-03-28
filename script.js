2eb09042466ff03294623a0a4a9a621adcd7c40d
CodeDreamerBen <91576401+CodeDreamerBen@users.noreply.github.com>
2022-03-28 14:04:06 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function getHexCode() {
+    document.getElementById("code").value = document.getElementById("hex").value;
+}
+function getColor() {
+    document.getElementById("color").value = document.getElementById("text").value;
+}
